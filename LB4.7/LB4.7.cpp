1d8b11700b749c24244c9f72a0f54ca85f8ce59c
OlehMalskyi <omalskij3@gmail.com>
2023-10-11 19:58:20 +0300

    lb4.7 commit

---
+++ LB4.7/LB4.7.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double xp, xk, x, dx, eps, a = 1, R, S;
+    int n;
+
+    cout << "xp = "; cin >> xp;
+    cout << "xk = "; cin >> xk;
+    cout << "dx = "; cin >> dx;
+    cout << "eps = "; cin >> eps;
+
+    cout << fixed;
+    cout << "-------------------------------------------------" << endl;
+    cout << "|" << setw(5) << "x" << " |"
+        << setw(10) << "cos(x)" << " |"
+        << setw(7) << "S" << " |"
+        << setw(5) << "n" << " |"
+        << endl;
+    cout << "-------------------------------------------------" << endl;
+
+    x = xp;
+    while (x <= xk)
+    {
+        n = 0;
+        a = 1;
+        S = a;
+        do {
+            n++;
+            R = pow(-1, n) * pow(x, 2 * n) / tgamma(2 * n + 1);
+            a = R;
+            S += a;
+        } while (abs(a) >= eps);
+
+        cout << "|" << setw(7) << setprecision(2) << x << " |"
+            << setw(10) << setprecision(5) << cos(x) << " |"
+            << setw(10) << setprecision(5) << S << " |"
+            << setw(5) << n << " |"
+            << endl;
+        x += dx;
+    }
+    cout << "-------------------------------------------------" << endl;
+
+    return 0;
+}
